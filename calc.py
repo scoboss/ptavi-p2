7cbbc0dea67ff412fad19af32afde0810cd3fe99
Sandra Cobos Sacido <scoboss@gamma23.aulas.gsyc.urjc.es>
2016-09-19 10:35:30 +0200

    calculadora modificada

--- calc.py
+++ calc.py
@@ -13,7 +13,17 @@ def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
 
+def mult(op1, op2):
+	"""Function to multi the operands"""
+	return op1 * op2
+
+def divid(op1, op2):
+	return op1 / op2
+
+
 if __name__ == "__main__":
+
+
     try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
@@ -24,6 +34,10 @@ if __name__ == "__main__":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
+	elif sys.argv[2] == "por":
+		result = mult(operando1, operando2)
+	elif sys.argv[2] == "division":
+		result = divid(operando1, operando2)
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
 
