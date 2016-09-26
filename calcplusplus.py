391fbe75fa497a4e8eee378222ec311580182b05
Sandra Cobos Sacido <sandra.12@outlook.es>
2016-09-26 23:45:13 +0200

    todos con pep8 correcto

--- calcplusplus.py
+++ calcplusplus.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3 
-# -*- coding: utf-8 -*- 
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import sys
 import csv
@@ -9,4 +9,3 @@ if __name__ == "__main__":
     fichero = sys.argv[1]
     with open(fichero) as ficheroejercicio:
         resultados_fichero(ficheroejercicio)
-
